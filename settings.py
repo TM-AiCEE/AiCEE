94ffbaf7e8ca3516b3f290eeb2d6de063b3651c8
msc0953 <msc0953@gmail.com>
2018-07-25 15:14:10 +0800

    remove default bot name

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 DEBUG = False
-TRAINING_MODE = False
+TRAINING_MODE = True
 TRAINING_SERVER_URL = r"ws://poker-training.vtr.trendnet.org:3001"
 BATTLE_SERVER_URL = r"ws://poker-battle.vtr.trendnet.org:3001"
-bot_name = "186dd1804a914d93a44a601e582f2195"
+bot_name = ""
